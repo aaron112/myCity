993b86a1a3cd0827e847f51c770b33e53cdcecd6
yyc009 <yyc009@ucsd.edu>
2013-03-09 18:33:06 -0800

    -pushed GAE backend files

---
+++ CodeLabEx1/src/com/google/appengine/codelab/GetUploadAddr.java
@@ -0,0 +1,27 @@
+package com.google.appengine.codelab;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+public class GetUploadAddr extends HttpServlet
+{
+	private BlobstoreService blobstoreService = BlobstoreServiceFactory
+	         .getBlobstoreService();
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+	         throws ServletException, IOException
+	{
+		String uploadpath = blobstoreService.createUploadUrl("/upload");
+		res.setHeader("Content-Type", "text/plain");
+		res.getWriter().write(uploadpath);
+		res.getWriter().flush();
+		res.getWriter().close();
+	}
+}
