5e36e2b95d0064bba8728e7270cda6c3a6ad2057
Aaron Wong <kicked1102@gmail.com>
2013-03-13 23:02:56 -0700

    Added design by contract.

--- myCity/src/edu/ucsd/mycity/chat/ChatRoom.java
+++ myCity/src/edu/ucsd/mycity/chat/ChatRoom.java
@@ -1,16 +1,10 @@
-/**
- * 
- */
 package edu.ucsd.mycity.chat;
 
 import java.util.ArrayList;
 
 import org.jivesoftware.smack.Chat;
-import org.jivesoftware.smack.MessageListener;
-import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 
 import android.util.Log;
@@ -18,25 +12,36 @@ import android.util.Log;
 import edu.ucsd.mycity.GTalkHandler;
 import edu.ucsd.mycity.buddy.BuddyEntry;
 
+
 /**
+ * Design by Contract
+ * @invariant title != null, isMultiUser is boolean, either sc or muc != null
  * @author Aaron
  *
  */
-
-public class ChatRoom implements PacketListener, MessageListener {
-	
+public class ChatRoom {
 	public class ChatMessage {
 		private BuddyEntry from;
 		private String msg;
 		
+		/**
+		 * @pre from is BuddyEntry, msg != null
+		 */
 		public ChatMessage( BuddyEntry from, String msg ) {
 			this.from = from;
 			this.msg = msg;
 		}
 		
+		/**
+		 * @post return null / BuddyEntry
+		 */
 		public BuddyEntry getFrom() {
 			return from;
 		}
+		
+		/**
+		 * @post return String
+		 */
 		public String getMsg() {
 			return msg;
 		}
@@ -47,66 +52,88 @@ public class ChatRoom implements PacketListener, MessageListener {
 	
 	private Chat sc = null;
 	private MultiUserChat muc = null;
-
-	private BuddyEntry participant;					// Single user mode: user addr, Multi-user mode: chatroom addr
-	private ArrayList<BuddyEntry> participants;		// Multi-user mode
+	
+	// Single user mode: user addr, Multi-user mode: chatroom addr
+	private BuddyEntry participant;
+	
+	// Multi-user mode
+	private ArrayList<BuddyEntry> participants = new ArrayList<BuddyEntry>();	
 	private ArrayList<ChatMessage> messages = new ArrayList<ChatMessage>();
 	
 	/**
-	 * Single user mode
+	 * Single-user mode
+	 * @pre newsc != null, participant != null
+	 * 
 	 */
 	public ChatRoom(Chat newsc, BuddyEntry participant) {
 		isMultiUser = false;
 		sc = newsc;
-		sc.addMessageListener(this);
 		
 		this.participant = participant;
 		this.title = participant.getName();
 	}
 	
 	/**
-	 * XMPPConnect needed in order to create 
-	 * @param connection
+	 * Multi-user mode
+	 * @pre newmuc != null, title != null
 	 */
 	public ChatRoom(MultiUserChat newmuc, String title) {
 		isMultiUser = true;
 		muc = newmuc;
 		
-		muc.addMessageListener(this);
 		this.title = title;
 	}
 	
+	/**
+	 * @post return boolean
+	 */
 	public boolean isMultiUser() {
 		return isMultiUser;
 	}
 	
+	/**
+	 * @post return String != null
+	 */
 	public String getTitle() {
+		if ( title == null )
+			return "";
 		return title;
 	}
+	
+	/**
+	 * @pre title != null
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 	
-	// Add a message to the message list
+	/**
+	 * Add a message to the message list
+	 * @pre contact is BuddyEntry (can be null), msg is String != null
+	 */
 	public void addMessage(BuddyEntry contact, String msg) {
 		messages.add( new ChatMessage(contact, msg) );
 	}
 	
+	/**
+	 * @pre newsc != null
+	 */
 	public void setChat(Chat newsc) {
-		if (newsc == null)
-			return;
-		
 		sc = newsc;
 	}
 	
+	/**
+	 * @pre newmuc != null
+	 */
 	public void setMulChat(MultiUserChat newmuc) {
-		if (newmuc == null)
-			return;
-		
 		muc = newmuc;
 	}
-
-	// Return boolean indicates if send is successful
+	
+	/**
+	 * Return boolean indicates if send is successful
+	 * @pre message != null
+	 * @post returns boolean
+	 */
 	public boolean sendMessage(String message) {
 		addMessage(null, message);	// contact = null indicates myself
 		
@@ -129,32 +156,10 @@ public class ChatRoom implements PacketListener, MessageListener {
 		return GTalkHandler.sendMessage(msg);
 	}
 	
-	@Override
-	// For multi-user only
-	public void processPacket(Packet packet) {
-		/***** Disabled
-		Message message = (Message) packet;
-		
-		if (message.getBody() == null)
-			return;
-		
-		addMessage( BuddyHandler.getBuddy( BuddyHandler.getBareAddr(message.getFrom()) ), message.getBody() );
-		*/
-	}
-
-	@Override
-	// For single-user only
-	public void processMessage(Chat chat, Message message) {
-
-		/***** Disabled
-		if (message.getBody() == null)
-			return;
-		
-		// New incoming message
-		addMessage( participant, message.getBody() );
-		 */
-	}
-
+	/**
+	 * @pre buddles != null, invitemsg != null
+	 * @post returns boolean
+	 */
 	public boolean addParticipants(ArrayList<BuddyEntry> buddies, String invitemsg) {
 		// Ignore request if in single-user mode
 		if ( !isMultiUser || muc == null )
@@ -167,21 +172,25 @@ public class ChatRoom implements PacketListener, MessageListener {
 		
 		return true;
 	}
-
+	
+	/**
+	 * @post returns BuddyEntry
+	 */
 	public BuddyEntry getParticipant() {
 		return participant;
 	}
 	
+	/**
+	 * @post returns ArrayList<BuddyEntry> != null
+	 */
 	public ArrayList<BuddyEntry> getParticipants() {
 		return participants;
 	}
 	
+	/**
+	 * @post returns ArrayList<ChatMessage> != null
+	 */
 	public ArrayList<ChatMessage> getMessages() {
 		return messages;
 	}
-
-
-	
-
-
 }
