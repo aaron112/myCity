b29d844185ac1ba811d130428ae13b939c414946
Aaron Wong <kicked1102@gmail.com>
2013-03-15 17:00:58 -0700

    One more clean-up.

--- myCity/src/edu/ucsd/mycity/AddUserContActivity.java
+++ myCity/src/edu/ucsd/mycity/AddUserContActivity.java
@@ -30,7 +30,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
